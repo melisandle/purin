b8a9a2b5768b49c16b9b3cdf88feb9a60a5b85d0
melisandle <42230482+melisandle@users.noreply.github.com>
2018-08-09 00:46:52 -0500

    Add files via upload

---
+++ purin.py
@@ -0,0 +1,27 @@
+#purin bot by mui
+
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import os 
+
+Client = discord.Client ()
+
+@client.event
+async def on_ready():
+    print ("woof woof!")
+
+#welcome message
+@client.event
+async def on_member_join(member):
+     embed = discord.Embed(title="welcome 🤝", description="**{0.name}** has joined the server.".format(member), color=0X836953)
+     await client.send_message(member.server.get_channel("476173627822440450"), embed=embed)
+
+#goodbye message
+@client.event
+async def on_member_remove(member):
+     embed = discord.Embed(title="goodbye 👋", description="**{0.name}** has left the server.".format(member), color=0X836953)
+     await client.send_message(member.server.get_channel("476173627822440450"), embed=embed)
+
+client.run(os.getenv('TOKEN'))
